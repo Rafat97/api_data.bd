ffe783d99dd796bee3b6656dc5c2254c6083c7b4
MohammadRakib <mohammadrakibzaman@gmail.com>
2020-08-27 22:12:52 +0600

    Instituition Table added

---
+++ api_data_bd/api_data_bd/Migrations/202008271546072_Instituition.cs
@@ -0,0 +1,36 @@
+﻿namespace api_data_bd.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Instituition : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Instituition",
+                c => new
+                {
+                    Id = c.String(nullable: false, maxLength: 128),
+                    Institute_Name = c.String(nullable: false, maxLength: 256),
+                    Institute_type = c.String(nullable: false, maxLength: 256),
+                    Establishment = c.String(nullable: false, maxLength: 256),
+                    Eiin = c.String(nullable: false, maxLength: 256),
+                    Mobile_Number = c.String( maxLength: 256),
+                    Management_type = c.String(nullable: false, maxLength: 256),
+                    Education_level = c.String(nullable: false, maxLength: 256),
+                   
+
+                })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.Id, unique: true, name: "InstituitionIdIndexing");
+        }
+        
+        public override void Down()
+        {
+            DropIndex("dbo.Instituition", "InstituitionIdIndexing");
+            DropTable("dbo.Instituition");
+           
+        }
+    }
+}
