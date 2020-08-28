07ffb4d8ba9b2cef5fc516e761fd9e90d537df0c
Rafat <rafathaque1997@gmail.com>
2020-08-28 13:08:42 +0600

    :+1:  add test Student Model

---
+++ api_data_bd/api_data_bd/Models/Students.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace api_data_bd.Models
+{
+    public class Students
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public int StudentAge { get; set; }
+    }
+}
