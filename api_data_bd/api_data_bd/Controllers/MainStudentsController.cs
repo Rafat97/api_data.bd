7ac667b11f4b49cf5318392a2ef07e3df0c86a92
Rafat <rafathaque1997@gmail.com>
2020-08-28 14:08:39 +0600

    :+1:  added view MainStudent

---
+++ api_data_bd/api_data_bd/Controllers/MainStudentsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using api_data_bd.Models;
+
+namespace api_data_bd.Controllers
+{
+    public class MainStudentsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: MainStudents
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Students.ToListAsync());
+        }
+
+        // GET: MainStudents/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Students students = await db.Students.FindAsync(id);
+            if (students == null)
+            {
+                return HttpNotFound();
+            }
+            return View(students);
+        }
+
+        // GET: MainStudents/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MainStudents/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "StudentId,StudentName,StudentAge")] Students students)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Students.Add(students);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(students);
+        }
+
+        // GET: MainStudents/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Students students = await db.Students.FindAsync(id);
+            if (students == null)
+            {
+                return HttpNotFound();
+            }
+            return View(students);
+        }
+
+        // POST: MainStudents/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "StudentId,StudentName,StudentAge")] Students students)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(students).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(students);
+        }
+
+        // GET: MainStudents/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Students students = await db.Students.FindAsync(id);
+            if (students == null)
+            {
+                return HttpNotFound();
+            }
+            return View(students);
+        }
+
+        // POST: MainStudents/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Students students = await db.Students.FindAsync(id);
+            db.Students.Remove(students);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
