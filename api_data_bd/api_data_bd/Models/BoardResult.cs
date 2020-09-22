7ca5ea9e246b585b312daba054cb9274ab45fca4
Rafat <rafathaque1997@gmail.com>
2020-09-23 05:43:16 +0600

    :+1:  Full Complete Update Dashboard

--- api_data_bd/api_data_bd/Models/BoardResult.cs
+++ api_data_bd/api_data_bd/Models/BoardResult.cs
@@ -10,24 +10,30 @@ namespace api_data_bd.Models
     public class BoardResult
     {
         [Key]
+        [Display(Name = "BoardResult Id")]
         public int BoardResultId { get; set; }
 
         [Required]
+        [Display(Name = "BoardResult Year")]
         public string Year { get; set; }
 
         [Required]
+        [Display(Name = "BoardResult ResultType")]
         public string ResultType { get; set; }
 
         [Required]
+        [Display(Name = "BoardResult ExamAttendence")]
         public string ExamAttendence { get; set; }
 
         [Required]
+        [Display(Name = "BoardResult GpaFiveStudentNumber")]
         public string GpaFiveStudentNumber { get; set; }
 
         [Required]
+        [Display(Name = "BoardResult FailStudentNumber")]
         public string FailStudentNumber { get; set; }
 
-
+        [Display(Name = "BoardResult Instituitions")]
         public int ? InstituitionId { get; set; }
         public virtual Instituitions Instituitions { get; set; }
 
--- api_data_bd/api_data_bd/Models/InstituteStatistics.cs
+++ api_data_bd/api_data_bd/Models/InstituteStatistics.cs
@@ -11,27 +11,31 @@ namespace api_data_bd.Models
 
 
         [Key]
+        [Display(Name = "InstituteStatistics Id")]
         public int InstituteStatisticsId { get; set; }
 
         [Required]
+        [Display(Name = "InstituteStatistics Male Teacher")]
         public string InstituteStatisticsMaleTeacher { get; set; }
 
 
         [Required]
+        [Display(Name = "InstituteStatistics FemaleTeacher")]
         public string InstituteStatisticsFemaleTeacher { get; set; }
 
 
         [Required]
+        [Display(Name = "InstituteStatistics Year")]
         public string InstituteStatisticsYear { get; set; }
 
-        
+        [Display(Name = "InstituteStatistics Boys Student")]
         public string InstituteStatisticsBoysStudent { get; set; }
 
 
-        
+        [Display(Name = "InstituteStatistics Girls Student")]
         public string InstituteStatisticsGirlsStudent { get; set; }
 
-
+        [Display(Name = "Instituition")]
         public int ? InstituitionId { get; set; }
         public virtual Instituitions Instituitions { get; set; }
 
